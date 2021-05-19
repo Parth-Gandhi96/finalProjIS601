5efad4e2ec50b8f2d1e3a73cee41a706f5c299fd
parth <gandhi.bapu96@gmail.com>
2021-05-18 23:13:16 -0400

    fix: updated test cases

--- app/test_app.py
+++ app/test_app.py
@@ -1,31 +1,17 @@
 import unittest
-from main import app
-import os
+import app
 
 
 class HomeViewTest(unittest.TestCase):
-
-    # def test_test(self):
-    #     assert app.test() == "Works!"
-
-    tempApp = None
+    temp_app = None
 
     @classmethod
     def setUpClass(cls):
-        cls.tempApp = app()
+        cls.temp_app = app()
 
-    def test_env_var(self):
-        try:
-            assert str(os.environ.get('SENDER_EMAIL_ADDRESS')) == "parth96g@gmail.com"
-        except AssertionError:
-            print("email is: ",str(os.environ.get('SENDER_EMAIL_ADDRESS')))
+    def setUp(self):
+        pass
 
-
-    def test_fetchMovieCollectionLast5Years(self):
-        try:
-            assert app.fetchMovieCollectionLast5Years() is not None
-        except AssertionError:
-            print("DB not loaded....")
 
 if __name__ == "__main__":
     unittest.main()
