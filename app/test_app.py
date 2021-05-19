2c2157df4ec4ac0bffb6f758b82ad8e076e0ab78
parth <gandhi.bapu96@gmail.com>
2021-05-18 22:27:45 -0400

    fix: updated test cases

--- app/test_app.py
+++ app/test_app.py
@@ -1,11 +1,18 @@
 import unittest
 import app
+import os
 
 
 class HomeViewTest(unittest.TestCase):
 
-    def test_test(self):
-        assert app.test() == "Works!"
+    # def test_test(self):
+    #     assert app.test() == "Works!"
+
+    def test_env_var(self):
+        assert str(os.environ.get('SENDER_EMAIL_ADDRESS'))=="parth96g@gmail.com"
+
+    def test_fetchMovieCollectionLast5Years(self):
+        assert app.fetchMovieCollectionLast5Years() is not None
 
 
 if __name__ == "__main__":
